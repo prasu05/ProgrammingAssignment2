840c4fb482016100863d710ce3ba237c9124d6f9
sushanta pradhan <sushant.research@gmail.com>
2014-04-25 17:19:15 +0530

    1. code for transforming an atomic matrix to a cacheable matrix 2. code for calculating inverse of a cacheable matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#converts an atomic matrix to a cacheable matrix whose inverse can be cached.
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	input <- x
+	set <- function(m){
+		input <<- m
+		i <<- NULL
+	}
+	get <- function() input
+	setinverse <- function(inverse) i <<- inverse
+	getinverse <- function() i
+	list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+#returns the inverse of a cacheable matrix; fetches the value from cache if present, else is freshly computed
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+	i <- x$getinverse()
+	if(!is.null(i)){
+		message('getting cached inverse')
+		return(i)
+	}
+	data <- x$get()
+	i <- solve(data)
+	x$setinverse(i)
+	i
 }
